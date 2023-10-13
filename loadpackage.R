c5050a4c2c7792cf0d07ebfc85603fe2ceccc1df
Caroline Lee <46834783+carolinelee78@users.noreply.github.com>
2023-10-13 14:09:14 -0500

    Create loadpackage.R

---
+++ loadpackage.R
@@ -0,0 +1,24 @@
+# detach all libraries
+
+detachAllPackages <- function() {
+  basic.packages <- c("package:stats", "package:graphics", "package:grDevices", "package:utils", "package:datasets", "package:methods", "package:base")
+  package.list <- search()[ifelse(unlist(gregexpr("package:", search()))==1, TRUE, FALSE)]
+  package.list <- setdiff(package.list, basic.packages)
+  if (length(package.list)>0)  for (package in package.list) detach(package,  character.only=TRUE)
+}
+detachAllPackages()
+
+# function to load libraries
+
+pkgTest <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[,  "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg,  dependencies = TRUE)
+  sapply(pkg,  require,  character.only = TRUE)
+}
+
+# loading packages 
+
+lapply(c("tidyverse"),pkgTest)
+library(tidyverse)
+ 
